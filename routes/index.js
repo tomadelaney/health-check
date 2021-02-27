e6d43b7be05c3f17732cbe62483500196abdc14f
Tom Delaney <tom.delaney@nyu.edu>
2021-02-26 20:40:35 -0500

    Initial health-check commit - see https://medium.com/kubernetes-tutorials/creating-liveness-probes-for-your-node-js-application-in-kubernetes-285fd6d22497

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+
+//var express = require('express');
+//var router = express.Router();
+
+///* GET home page. */
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
+
+//module.exports = router;
+
+'use strict';
+const express = require('express');
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+app.get('/health-check',(req,res)=> {
+  res.send ("Health check passed");
+});
+app.get('/bad-health',(req,res)=> {
+  res.status(500).send('Health check did not pass');
+});
+app.listen(PORT, HOST);
+console.log(Running on http://${HOST}:${PORT});
