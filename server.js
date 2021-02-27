b22a9e478d6801da79f0dde1316df6c37e177607
Tom Delaney <tom.delaney@nyu.edu>
2021-02-27 12:41:52 -0500

    Tried to match up default /healthz at 10256

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 'use strict';
 const express = require('express');
 // Constants
-const PORT = 80;
+const PORT = 10256;
 const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
     res.status(200).send('/ Health Check Passed');
+});
+app.get('/healthz',(req,res)=> {
+    res.status(200).send ("/health-check Health check passed");
 });
 app.get('/health-check',(req,res)=> {
     res.status(200).send ("/health-check Health check passed");
